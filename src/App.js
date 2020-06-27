7c3f61768b43339bd2ac4064cd9bcc35f7dbcdd2
pjchender <pjchender@gmail.com>
2020-06-27 10:22:29 +0800

    create currentWeather with useState

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import styled from '@emotion/styled';
 import { ThemeProvider } from 'emotion-theming';
+import dayjs from 'dayjs';
 
 import { ReactComponent as DayCloudyIcon } from './images/day-cloudy.svg';
 import { ReactComponent as AirFlowIcon } from './images/airFlow.svg';
@@ -127,27 +128,41 @@ const Refresh = styled.div`
 
 const App = () => {
   const [currentTheme, setCurrentTheme] = useState('light');
+  // STEP 2：定義會使用到的資料狀態
+  const [currentWeather, setCurrentWeather] = useState({
+    observationTime: '2020-12-12 22:10:00',
+    locationName: '臺北市',
+    description: '多雲時晴',
+    windSpeed: 3.6,
+    temperature: 32.1,
+    rainPossibility: 60,
+  });
 
   return (
     <ThemeProvider theme={theme[currentTheme]}>
       <Container>
         <WeatherCard>
-          <Location>台北市</Location>
-          <Description>多雲時晴</Description>
+          <Location>{currentWeather.locationName}</Location>
+          <Description>{currentWeather.description}</Description>
           <CurrentWeather>
             <Temperature>
-              23 <Celsius>°C</Celsius>
+              {Math.round(currentWeather.temperature)} <Celsius>°C</Celsius>
             </Temperature>
             <DayCloudy />
           </CurrentWeather>
           <AirFlow>
-            <AirFlowIcon /> 23 m/h
+            <AirFlowIcon /> {currentWeather.windSpeed} m/h
           </AirFlow>
           <Rain>
-            <RainIcon /> 48%
+            <RainIcon /> {currentWeather.rainPossibility}%
           </Rain>
           <Refresh>
-            最後觀測時間：上午 12:03 <RefreshIcon />
+            最後觀測時間：
+            {new Intl.DateTimeFormat('zh-TW', {
+              hour: 'numeric',
+              minute: 'numeric',
+            }).format(dayjs(currentWeather.observationTime))}{' '}
+            <RefreshIcon />
           </Refresh>
         </WeatherCard>
       </Container>
